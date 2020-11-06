ca81f34982ad5d885c640c9eb1c86a38816fefc0
awanaprilino <aprilinoawan@gmail.com>
2020-11-06 22:30:04 +0700

    perbaikan contact

--- pages/p/contact.js
+++ pages/p/contact.js
@@ -19,7 +19,7 @@ export default function contact() {
                 <section className="w-full md:w-2/3 flex flex-col items-center px-3">
                     <article className="w-full flex flex-col shadow my-4">
                         <div className="bg-white flex flex-col justify-start p-6">
-                            <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">About</a>
+                            <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Contact</a>
                             <p href="#" className="text-sm">
                                 By <a href="#" className="font-semibold hover:text-gray-800">Awan</a></p>
                             <div className="post">
